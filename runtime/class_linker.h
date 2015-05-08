rovo89/android_art

Remove reference_static_offsets used for iterating through class static fields.
Since static fields are contiguous in class object and there is no need to
traverse super classes, it's not meaningful to use reference_static_offsets.
Also especially with embedded vtable/imt, static field offset can't be encoded
with an unsigned integer anyway.

A corresponding change is made to Class.java to remove the member field.

Bug: 16236588
Change-Id: I1fde3cd9efce884945876f0658c63d992164fd94

runtime/class_linker.h
@@ -533,10 +533,7 @@ class ClassLinker {
       SHARED_LOCKS_REQUIRED(Locks::mutator_lock_);
   void CreateReferenceInstanceOffsets(ConstHandle<mirror::Class> klass)
       SHARED_LOCKS_REQUIRED(Locks::mutator_lock_);
-  void CreateReferenceStaticOffsets(ConstHandle<mirror::Class> klass)
-      SHARED_LOCKS_REQUIRED(Locks::mutator_lock_);
-  void CreateReferenceOffsets(ConstHandle<mirror::Class> klass, bool is_static,
-                              uint32_t reference_offsets)
+  void CreateReferenceOffsets(ConstHandle<mirror::Class> klass, uint32_t reference_offsets)
       SHARED_LOCKS_REQUIRED(Locks::mutator_lock_);
 
   // For use by ImageWriter to find DexCaches for its roots

quanganh2627/baytrail-i-x64-kitkat_art

Conflicts Resolved:
	runtime/class_linker.cc
	runtime/class_linker.h
	runtime/class_linker_test.cc
Depends-Change-Id: Icebdf0febee72a5dbbebc1906b069d52bf756e80
Category: device enablement
Domain: AOSP.ART-VM
Upstream-Candidate: no, from AOSP
Origin: https://android-review.googlesource.com/#/c/107304/
Tracked-On: https://jira01.devtools.intel.com/browse/CAR-1611
Signed-off-by: Haitao Feng <haitao.feng@intel.com>

runtime/class_linker.h
@@ -575,10 +575,7 @@ class ClassLinker {
 
   void CreateReferenceInstanceOffsets(Handle<mirror::Class> klass)
       SHARED_LOCKS_REQUIRED(Locks::mutator_lock_);
-  void CreateReferenceStaticOffsets(Handle<mirror::Class> klass)
-      SHARED_LOCKS_REQUIRED(Locks::mutator_lock_);
-  void CreateReferenceOffsets(Handle<mirror::Class> klass, bool is_static,
-                              uint32_t reference_offsets)
+  void CreateReferenceOffsets(Handle<mirror::Class> klass, uint32_t reference_offsets)
       SHARED_LOCKS_REQUIRED(Locks::mutator_lock_);
 
   // For use by ImageWriter to find DexCaches for its roots