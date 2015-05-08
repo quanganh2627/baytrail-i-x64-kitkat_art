rovo89/android_art

Avoid abort in malformed dex code.
Don't allow a perceived double monitor-enter on a register
to abort libartd.
Allow expected verifier errors in the smali tests.
Tidy includes in the method verifier.
Bug: 17978759

Change-Id: Ic44924c788cd2334f91a047fb41b459b89a1843b

runtime/verifier/register_line.cc
@@ -310,8 +310,12 @@ void RegisterLine::PushMonitor(MethodVerifier* verifier, uint32_t reg_idx, int32
     verifier->Fail(VERIFY_ERROR_BAD_CLASS_HARD) << "monitor-enter stack overflow: "
         << monitors_.size();
   } else {
-    SetRegToLockDepth(reg_idx, monitors_.size());
-    monitors_.push_back(insn_idx);
+    if (SetRegToLockDepth(reg_idx, monitors_.size())) {
+      monitors_.push_back(insn_idx);
+    } else {
+      verifier->Fail(VERIFY_ERROR_BAD_CLASS_HARD) << "unexpected monitor-enter on register v" <<
+          reg_idx;
+    }
   }
 }
 

quanganh2627/baytrail-i-x64-kitkat_art

JIRA: https://jira01.devtools.intel.com/browse/GMINL-6172
Category: device enablement
Domain: AOSP.ART-Quick
Upstream-Candidate: no
Tracked-On: https://jira01.devtools.intel.com/browse/GMINL-6172
Origin: https://android-review.googlesource.com/#/c/114661/
Depends-Change-Id: I379ed23434c72a91e875e753708387be6502bc57

runtime/verifier/register_line.cc
@@ -419,8 +419,12 @@ void RegisterLine::PushMonitor(uint32_t reg_idx, int32_t insn_idx) {
   } else if (monitors_.size() >= 32) {
     verifier_->Fail(VERIFY_ERROR_BAD_CLASS_HARD) << "monitor-enter stack overflow: " << monitors_.size();
   } else {
-    SetRegToLockDepth(reg_idx, monitors_.size());
-    monitors_.push_back(insn_idx);
+    if (SetRegToLockDepth(reg_idx, monitors_.size())) {
+      monitors_.push_back(insn_idx);
+    } else {
+      verifier_->Fail(VERIFY_ERROR_BAD_CLASS_HARD) << "unexpected monitor-enter on register v" <<
+          reg_idx;
+    }
   }
 }
 